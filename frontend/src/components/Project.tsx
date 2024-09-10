5296f1369de58d5649d4dfb9585b10b9bf8dd923
Andrew Stormer <100243512+AndrewStormer@users.noreply.github.com>
2024-09-10 12:22:25 -0500

    feat(projects): created projects page
    feat(project): redid project component
    
    fix(dependencies): added overrides

--- frontend/src/components/Project.tsx
+++ frontend/src/components/Project.tsx
@@ -1,28 +1,37 @@
-import React, { useState, useEffect } from 'react'
-//import { useParams } from 'react-router-dom'
+import { ProjectXSkillDto } from "../pages/Projects";
 
-export interface ProjectDto {
-  id: number,
-  name: string,
-  description: string,
-  status: string,
-  github?: string,
-  video?: string
-}
+const github = require('../github_logo.png');
 
-export default function Project() {
-  //let params = useParams()
-  const [data, setData] = useState()
+export default function Project({project}) {
+  const video = require(`../${project?.video}`)
 
-  useEffect(() => {
-    fetch(`http://localhost:3001/project`)
-      .then(response => response.json())
-      .then(json => setData(json))
-      .catch(error => console.error(error))
-  }, [])
-  if (data != null) {
-    return <div>{(data as ProjectDto)?.name}</div>
-  } else {
-    return <div></div>
-  }
+  return (
+    <div className="projectContainer">
+      <div className="projectName">
+        <h1>{project?.name}</h1>
+        {project?.status}
+      </div>
+      <div className="githubContainer">
+        <a href={project?.github}>
+          <img src={github} alt={'github'}></img>
+        </a>
+      </div>
+      <p>
+        {project?.description}
+      </p>
+      <div className='projectSkillsContainer'>
+        <h4>Skills used/gained during this project:</h4>
+        <div className='projectSkillsGridContainer'>
+          {project?.skills?.map((s: ProjectXSkillDto) => (
+            <li>
+              {s.skill_id.skill}
+            </li>
+            ))}
+        </div>
+      </div>
+      <div className='projectVideoContainer'>
+        <img src={video} alt={'this is a picture of a project'} />
+      </div>
+    </div>
+  )
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import About from '../components/About'
 import RelevantCourseWork from '../components/RelevantCourseWork'
 
