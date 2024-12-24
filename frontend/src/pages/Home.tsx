1028ce712bfdeae97eff780d2fe1240d74e480d6
Andrew Stormer <100243512+AndrewStormer@users.noreply.github.com>
2024-12-23 21:08:48 -0600

    feat(styling): added tailwind styling

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,29 @@
+import { useEffect } from 'react';
 import About from '../components/About'
+import Greeting from '../components/Greeting';
 import RelevantCourseWork from '../components/RelevantCourseWork'
 import Skills from '../components/Skills'
+import Projects, { ProjectDto } from './Projects';
+const backgroundImage = require('../background.png');
+import Header from '../components/Header'
 
 const pdf = require('..//Andrew_Stormer_Resume_FL2024.pdf');
 
-export default function Home() {
-  return (
-    <div className='homeContainer'>
-      <div className='homeContentContainer'>
-        <About />
-        <RelevantCourseWork />
-        <Skills />
-        <a href={pdf} download>
-          Download and view my resume
-        </a>
+export default function Home() {  return (
+    <div 
+      className='subpixel-antialiased font-sans w-screen scroll-smooth'>
+      <div className='fixed object-top pb-16 md:mb-40'>
+        <Header />
       </div>
+      <Greeting />
+      <div id="section1" >
+        <About />
+      </div>
+      <Projects />
     </div>
   )
 }
+function setData(json: any): any {
+  throw new Error('Function not implemented.');
+}
+
