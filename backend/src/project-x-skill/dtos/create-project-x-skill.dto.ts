964b29953dff4e94f69682b30045571d0304277b
Andrew Stormer <100243512+AndrewStormer@users.noreply.github.com>
2024-12-26 14:51:16 -0600

    test: removed backend

--- backend/src/project-x-skill/dtos/create-project-x-skill.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateProjectXSkillDto {
-  @ApiProperty({ name: 'skill_id', type: Number, required: true })
-  skill_id: number;
-
-  @ApiProperty({ name: 'project_id', type: Number, required: true })
-  project_id: number;
-}
