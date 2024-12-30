87178e379059a38fd43430d0de4341aec907eb7c
Andrew Stormer <100243512+AndrewStormer@users.noreply.github.com>
2024-12-30 16:09:32 -0600

    test

--- frontend/src/components/RelevantCourseWork.tsx
+++ frontend/src/components/RelevantCourseWork.tsx
@@ -28,8 +28,9 @@ export interface Course {
 
 export default function RelevantCourseWork() {
   const [data, setData] = useState();
+  
   useEffect(() => {
-    fetch(`https://fix-amplfiy.dq50hk79mboni.amplifyapp.com/course`)
+    getCourses()
       .then(response => response.json())
       .then(json => setData(json))
       .catch(error => console.error(error))
