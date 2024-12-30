65914d3f8586bd0ffd358f23a67c0a33c0876c2e
Andrew Stormer <100243512+AndrewStormer@users.noreply.github.com>
2024-12-30 08:20:15 -0600

    fix

--- frontend/amplify/functions/server/resource.ts
+++ frontend/amplify/functions/server/resource.ts
@@ -2,5 +2,5 @@ import { defineFunction } from "@aws-amplify/backend";
 
 export const myApiFunction = defineFunction({
   name: "api-server",
-  entry: "./backend/src/index.ts",
+  entry: "./backend/src/lambda.ts",
 });
